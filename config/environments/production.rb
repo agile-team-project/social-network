08c2fc0386efd9d2818f0e597766137ae2470992
Cory Selvidge <healincheese@gmail.com>
2019-09-12 15:26:59 -0700

    Travis CI yml added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'Get host URL for heroku' }
+  config.action_mailer.default_url_options = { host: 'https://social-network-team-project.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
