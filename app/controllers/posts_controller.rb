aa9a59fd27f0b71bce83b383c4d5e1981eaf7560
Majun1990 <merijun_a@hotmail.com>
2019-10-02 13:05:15 +0000

    added end on index method of post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,8 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
     def index
       @posts = Post.all
-
+    end
+    
     def new
         @post = Post.new
     end
