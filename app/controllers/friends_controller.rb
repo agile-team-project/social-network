d364cb7d354bc2b82ae74ad4dbc03325e07cc00b
Brian Johnson <brianjohnsona@yahoo.com>
2019-10-03 00:23:33 +0000

    added models

---
+++ app/controllers/friends_controller.rb
@@ -0,0 +1,29 @@
+class FriendsController < ApplicationController
+    def index
+        # @friends = Friend.includes(:user).where(user: current_user).order('Users.name')
+        @friends = current_user.friends.order('Users.name')
+    end
+
+    # /friends/search?q=john
+    def search
+        q = params[:q]
+        @possible_friends = User.with_name_or_email(q).order(:name)
+    end
+
+    def create
+        # @post = current_user.posts.create(post_params)
+    end
+
+
+    def destroy
+        # @post = Post.find(params[:id])
+    end
+
+  
+
+    private
+
+    def user_params
+        params.require(:friend).permit(:friend_id)
+    end
+end
