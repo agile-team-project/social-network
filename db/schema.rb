0ba6a59970f3d96b9397b10c0d66a325710403cf
Cory Selvidge <healincheese@gmail.com>
2019-10-03 04:54:27 -0700

    Adding group creation and basic testing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_20_224148) do
+ActiveRecord::Schema.define(version: 2019_09_27_225356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2019_09_20_224148) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
+    t.index ["name"], name: "index_groups_on_name"
   end
 
   create_table "groups_users", force: :cascade do |t|
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,3 +1,3 @@
 FactoryBot.define do
-  
+
 end
